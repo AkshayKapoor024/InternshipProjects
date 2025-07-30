eb6a47ee6dcdc2331f75327534b009ceabdfc638
AKSHAY KAPOOR <work.akshaykapoor24@gmail.com>
2025-07-30 15:32:19 +0530

    Update navbar.jsx

--- Database/myapp/src/components/navbar.jsx
+++ Database/myapp/src/components/navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
       await signOut(auth);
       console.log('User logged out');
       toast.success('User Logout Successful')
-      navigate('/signup')
+      navigate('/login')
     } catch (error) {
       toast.error('User logout failed ! try again')
       console.error('Error:', error);
@@ -28,7 +28,7 @@ export default function Navbar() {
         setUser(user)
          setUserLoaded(true);
 
-        toast.success(`Logged in as ${user.displayName}`)
+        toast.success(`Logged in as ${user.email}`)
         console.log('🔥 Logged-in user:', user);
       } else {
         toast.error('Log in first to access this section')
@@ -49,13 +49,11 @@ useEffect(()=>{
     <div className=' h-[10vh] w-full flex items-center justify-between xl:p-5 bg-[#15151575] '>
    <Link to='/'><div className='text-sm md:text-2xl xl:text-3xl font-bold h-full mr-2 flex justify-center items-center'>🔥Database Handler</div></Link>
       <div className='flex justify-center items-center gap-10'>
-        <div className='text-sm md:text-2xl xl:text-2xl font-bold xl:w-auto h-[10vh] flex justify-center items-center'>{`Greetings!! ${user.displayName}`}</div>
+        <div className='text-sm md:text-2xl xl:text-2xl font-bold xl:w-auto h-[10vh] flex justify-center items-center'>{`Greetings!! ${user.email}`}</div>
         <div className=' w-[80px] h-[45px] xl:w-[70px] xl:h-[70px] rounded-full flex justify-center items-center'>
           <img
-            src={user?.photoURL}
-            onError={(e) => {
-              e.currentTarget.src = "https://img.daisyui.com/images/profile/demo/yellingwoman@192.webp";
-            }}
+            src={user.photoURL? user.photoURL: "https://img.daisyui.com/images/profile/demo/yellingwoman@192.webp"
+            }
             alt={user?.displayName || 'User'}
             className="w-full h-full rounded-full object-cover"
           />
