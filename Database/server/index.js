9b1d7c48ba05f9b1e50410a232da3bdaacf31ed6
AKSHAY KAPOOR <work.akshaykapoor24@gmail.com>
2025-07-30 12:20:58 +0530

    Update index.js

--- Database/server/index.js
+++ Database/server/index.js
@@ -33,7 +33,7 @@ const pool = mysql.createPool({
   multipleStatements: true                // unlimited queued requests
 });
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://importify-iota.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
